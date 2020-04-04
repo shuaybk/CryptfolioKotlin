e8f7056e478fd12011423c7e276ac81a9b955b51
Shuayb Khan <shuaybk@gmail.com>
2020-04-04 17:38:35 -0400

    Add functionality for bottom navigation bar.  Respective fragments are displayed when navigation item is selected

--- app/src/main/java/com/shuayb/capstone/android/cryptfoliokotlin/MainActivity.kt
+++ app/src/main/java/com/shuayb/capstone/android/cryptfoliokotlin/MainActivity.kt
@@ -2,11 +2,38 @@ package com.shuayb.capstone.android.cryptfoliokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById(R.id.bottom_nav_bar) as BottomNavigationView
+
+        bottomNav.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener {item->
+            val selectedFragment : Fragment
+
+            when (item.itemId) {
+                R.id.nav_market -> {
+                    selectedFragment = MarketsFragment()
+                }
+                R.id.nav_portfolio -> {
+                    selectedFragment = PortfolioFragment()
+                }
+                R.id.nav_settings -> {
+                    selectedFragment = SettingsFragment()
+                }
+                else -> {
+                    return@OnNavigationItemSelectedListener false
+                }
+            }
+
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+
+            return@OnNavigationItemSelectedListener true
+        })
     }
 }
