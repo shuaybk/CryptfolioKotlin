87b6c3e0f24405a013366862d36c6a1598f6d10e
Shuayb Khan <shuaybk@gmail.com>
2020-04-06 18:47:31 -0400

    Populate Settings fragment with "Refresh Interval" setting

--- app/src/main/java/com/shuayb/capstone/android/cryptfoliokotlin/SettingsFragment.kt
+++ app/src/main/java/com/shuayb/capstone/android/cryptfoliokotlin/SettingsFragment.kt
@@ -5,14 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
 }
