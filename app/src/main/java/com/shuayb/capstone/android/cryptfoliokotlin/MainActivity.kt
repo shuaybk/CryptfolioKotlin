4456967548bcaa603a4d8df8ac411c204d32f323
Shuayb Khan <shuaybk@gmail.com>
2020-04-06 17:59:33 -0400

    Add DataBinding.  Initialize to show the Markets view first.

--- app/src/main/java/com/shuayb/capstone/android/cryptfoliokotlin/MainActivity.kt
+++ app/src/main/java/com/shuayb/capstone/android/cryptfoliokotlin/MainActivity.kt
@@ -2,37 +2,49 @@ package com.shuayb.capstone.android.cryptfoliokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.shuayb.capstone.android.cryptfoliokotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mBinding : ActivityMainBinding
+    private lateinit var marketsFragment : MarketsFragment
+    private lateinit var portfolioFragment : PortfolioFragment
+    private lateinit var settingsFragment: SettingsFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val bottomNav = findViewById(R.id.bottom_nav_bar) as BottomNavigationView
+        initSetup()
+    }
 
-        bottomNav.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener {item->
+    private fun initSetup() {
+        marketsFragment = MarketsFragment()
+        portfolioFragment = PortfolioFragment()
+        settingsFragment = SettingsFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, marketsFragment).commit()
+
+        mBinding.bottomNavBar.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener {item->
             val selectedFragment : Fragment
 
             when (item.itemId) {
                 R.id.nav_market -> {
-                    selectedFragment = MarketsFragment()
+                    selectedFragment = marketsFragment
                 }
                 R.id.nav_portfolio -> {
-                    selectedFragment = PortfolioFragment()
+                    selectedFragment = portfolioFragment
                 }
                 R.id.nav_settings -> {
-                    selectedFragment = SettingsFragment()
+                    selectedFragment = settingsFragment
                 }
                 else -> {
                     return@OnNavigationItemSelectedListener false
                 }
             }
-
             supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
-
             return@OnNavigationItemSelectedListener true
         })
     }
